f877325aa3c8c42313f2ce1cbcfb48651f72e4ca
zhanna-naumenko <jannetj88@gmail.com>
2024-02-24 20:35:43 +0200

    Added exercises of Basics Of Python

--- test1.py
+++ test1.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+name = "zhanna"
+print(name.upper())
+num1 = 4
+num2 = 6
+print(num1 + num2)
+
+list1 = [5, 10, 15, 20, 25, 50, 20]
+list1[list1.index(20)] = 200
+print(list1)
+
+color = input('Please input one of the color "red", "green" or "yellow":  ')
+if color == "red":
+    print("Stop driving!")
+elif color == "green":
+    print("You can drive!")
+elif color == "yellow":
+    print("Slow down!")
+else:
+    print("The value is not defined!")
